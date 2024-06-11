eb70a69f0296f75f76d2aa3f8bb6f25c45432f98
aagogue <aagogue@iut.univ-paris8.fr>
2024-06-11 14:54:37 +0200

    un commit manquant refait +responsive sur les bouttons

--- src/main/java/com/letop3/ktsh/Main.java
+++ src/main/java/com/letop3/ktsh/Main.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.util.Objects;
 
@@ -21,6 +22,24 @@ public class Main extends Application {
 
         Scene scene = new Scene(root);
         SceneManager.getInstance().setMainScene(scene);
+
+        // Fix the window size
+        primaryStage.setWidth(960);
+        primaryStage.setHeight(539);
+        primaryStage.setResizable(false);
+
+        // Remove the title bar and borders
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        // Set fullscreen toggle on F11 without a message and remove ESC hint
+        primaryStage.setFullScreenExitHint("");
+        scene.setOnKeyPressed(event -> {
+            switch (event.getCode()) {
+                case F11:
+                    primaryStage.setFullScreen(!primaryStage.isFullScreen());
+                    break;
+            }
+        });
 
         primaryStage.setTitle("Kill To Save Her");
         primaryStage.setScene(scene);
--- src/main/java/com/letop3/ktsh/controller/MainMenuController.java
+++ src/main/java/com/letop3/ktsh/controller/MainMenuController.java
@@ -4,9 +4,11 @@ import com.letop3.ktsh.view.MainMenuView;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,19 +17,46 @@ public class MainMenuController implements Initializable {
     @FXML
     private AnchorPane exitPopupPane, creditsPopUpPane;
     @FXML
-    private StackPane imagePane;
+    private VBox mainMenu;
+    @FXML
+    private StackPane mainPane;
     @FXML
     private ImageView backgroundImage;
     private MainMenuView menuView;
+    @FXML
+    private ImageView logoImageView;
+    @FXML
+    private VBox buttonVBox;
+    @FXML
+    private Button button1, button2, button3, button4;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        backgroundImage.fitHeightProperty().bind(imagePane.heightProperty());
-        backgroundImage.fitWidthProperty().bind(imagePane.widthProperty());
-        menuView = new MainMenuView();
-        menuView.playMusic("src/main/resources/com/letop3/ktsh/audio/music/menu2.mp3");
-    }
+        // Lier la taille de l'image de fond
+        backgroundImage.fitWidthProperty().bind(mainPane.widthProperty());
+        backgroundImage.fitHeightProperty().bind(mainPane.heightProperty());
 
+        // Lier la taille de l'image du logo
+        logoImageView.fitWidthProperty().bind(mainPane.widthProperty().multiply(0.2));
+        logoImageView.fitHeightProperty().bind(mainPane.heightProperty().multiply(0.2));
+
+        // Lier la VBox interne à la taille du mainPane
+        buttonVBox.prefWidthProperty().bind(mainPane.widthProperty().multiply(0.5));
+        buttonVBox.prefHeightProperty().bind(mainPane.heightProperty().multiply(0.4));
+
+        // Lier la taille des boutons
+        button1.prefWidthProperty().bind(buttonVBox.prefWidthProperty().multiply(0.8));
+        button1.prefHeightProperty().bind(buttonVBox.prefHeightProperty().multiply(0.2));
+        button2.prefWidthProperty().bind(buttonVBox.prefWidthProperty().multiply(0.8));
+        button2.prefHeightProperty().bind(buttonVBox.prefHeightProperty().multiply(0.2));
+        button3.prefWidthProperty().bind(buttonVBox.prefWidthProperty().multiply(0.8));
+        button3.prefHeightProperty().bind(buttonVBox.prefHeightProperty().multiply(0.2));
+        button4.prefWidthProperty().bind(buttonVBox.prefWidthProperty().multiply(0.8));
+        button4.prefHeightProperty().bind(buttonVBox.prefHeightProperty().multiply(0.2));
+
+        menuView = new MainMenuView();
+        menuView.playMusic("src/main/resources/com/letop3/ktsh/audio/music/menu.mp3");
+    }
 
     @FXML
     public void startGame() {
