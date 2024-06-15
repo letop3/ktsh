ab9655a7b06d37ec40f2f58a7e8efd11c983b393
aagogue <164860332+aagogue@users.noreply.github.com>
2024-06-15 23:44:38 +0200

    qwick fix flute enchantée
    Signed-off-by: aagogue <164860332+aagogue@users.noreply.github.com>

--- src/main/java/com/letop3/ktsh/model/item/artefact/FluteEnchantee.java
+++ src/main/java/com/letop3/ktsh/model/item/artefact/FluteEnchantee.java
@@ -4,14 +4,19 @@ import com.letop3.ktsh.model.entity.player.Player;
 import com.letop3.ktsh.view.music.SoundPlayer;
 
 public class FluteEnchantee extends Artefact {
+    private boolean actived;
     public FluteEnchantee(int id, String nom, String description, int prix) {
         super(id, nom, description, prix);
         this.setIcon("/com/letop3/ktsh/images/item/flute.png");
-
+        this.actived = false;
     }
 
     @Override
     public void action(Player player) {
-        SoundPlayer.changeBackgroundMusic("src/main/resources/com/letop3/ktsh/audio/music/troll.mp3");
+        if (!actived) {
+            SoundPlayer.changeBackgroundMusic("src/main/resources/com/letop3/ktsh/audio/music/troll.mp3");
+            actived = true;
+            System.out.println("Flute enchantée activée");
+        }
     }
 }
