ee40b9aad11b963cc3453981d4ad5e48a3ef92fd
aagogue <164860332+aagogue@users.noreply.github.com>
2024-06-15 20:03:07 +0200

    bypass du menu en attendant modification dans mainmenu avec petite animation sympa
    Signed-off-by: aagogue <164860332+aagogue@users.noreply.github.com>

--- src/main/java/com/letop3/ktsh/controller/MainMenuController.java
+++ src/main/java/com/letop3/ktsh/controller/MainMenuController.java
@@ -2,11 +2,13 @@ package com.letop3.ktsh.controller;
 
 import com.letop3.ktsh.view.MainMenuView;
 import javafx.application.Platform;
-import javafx.beans.binding.Bindings;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -18,12 +20,11 @@ public class MainMenuController implements Initializable {
     @FXML
     private VBox exitPopupVBox, creditsPopupVBox, creditsTextVBox, creditsContentVBox;
     @FXML
-    private VBox mainMenuVBox;
+    private VBox mainMenuVBox, loadingVbox;
     @FXML
     private StackPane mainPane;
     @FXML
     private ImageView backgroundImageView;
-    private MainMenuView menuView;
     @FXML
     private ImageView logoImageView;
     @FXML
@@ -33,95 +34,65 @@ public class MainMenuController implements Initializable {
     @FXML
     private Button yesExitButton, noExitButton, closeCreditsButton;
     @FXML
-    private Label exitLabel, creditsTitleLabel;
+    private Label exitLabel, creditsTitleLabel, loadingLabel;
     @FXML
     private Label credit1, credit2, credit3;
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
+    private ProgressIndicator loadingIndicator;
 
-    private boolean isPopupOpen = false; // Nouveau booléen pour suivre l'état des popups
+    private boolean isPopupOpen = false;
+    private MainMenuView menuView;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // Lier la taille de l'image de fond
-        backgroundImageView.fitWidthProperty().bind(mainPane.widthProperty());
-        backgroundImageView.fitHeightProperty().bind(mainPane.heightProperty());
-
-        // Lier la taille de l'image du logo
-        logoImageView.fitWidthProperty().bind(mainPane.widthProperty().multiply(0.2));
-        logoImageView.fitHeightProperty().bind(mainPane.heightProperty().multiply(0.2));
-
-        // Lier la VBox interne à la taille du mainPane
-        buttonContainerVBox.prefWidthProperty().bind(mainPane.widthProperty().multiply(0.5));
-        buttonContainerVBox.prefHeightProperty().bind(mainPane.heightProperty().multiply(0.4));
-
-        // Lier la taille des boutons
-        startGameButton.prefWidthProperty().bind(buttonContainerVBox.prefWidthProperty().multiply(0.8));
-        startGameButton.prefHeightProperty().bind(buttonContainerVBox.prefHeightProperty().multiply(0.2));
-        settingsButton.prefWidthProperty().bind(buttonContainerVBox.prefWidthProperty().multiply(0.8));
-        settingsButton.prefHeightProperty().bind(buttonContainerVBox.prefHeightProperty().multiply(0.2));
-        creditsButton.prefWidthProperty().bind(buttonContainerVBox.prefWidthProperty().multiply(0.8));
-        creditsButton.prefHeightProperty().bind(buttonContainerVBox.prefHeightProperty().multiply(0.2));
-        exitButton.prefWidthProperty().bind(buttonContainerVBox.prefWidthProperty().multiply(0.8));
-        exitButton.prefHeightProperty().bind(buttonContainerVBox.prefHeightProperty().multiply(0.2));
-
-        // Lier la taille et la position des popups avec des limites maximales
-        exitPopupVBox.prefWidthProperty().bind(mainPane.widthProperty().multiply(0.5));
-        exitPopupVBox.prefHeightProperty().bind(mainPane.heightProperty().multiply(0.4));
-        exitPopupVBox.maxWidthProperty().setValue(800);  // Valeur maximale de la largeur
-        exitPopupVBox.maxHeightProperty().setValue(400); // Valeur maximale de la hauteur
-
-        creditsPopupVBox.prefWidthProperty().bind(mainPane.widthProperty().multiply(0.5));
-        creditsPopupVBox.prefHeightProperty().bind(mainPane.heightProperty().multiply(0.4));
-        creditsPopupVBox.maxWidthProperty().setValue(1000);  // Valeur maximale de la largeur
-        creditsPopupVBox.maxHeightProperty().setValue(600); // Valeur maximale de la hauteur
-
-        // Lier la taille des boutons et du label dans exitPopupVBox
-        yesExitButton.prefWidthProperty().bind(exitPopupVBox.prefWidthProperty().multiply(0.3));
-        yesExitButton.prefHeightProperty().bind(exitPopupVBox.prefHeightProperty().multiply(0.2));
-        noExitButton.prefWidthProperty().bind(exitPopupVBox.prefWidthProperty().multiply(0.3));
-        noExitButton.prefHeightProperty().bind(exitPopupVBox.prefHeightProperty().multiply(0.2));
-        exitLabel.prefWidthProperty().bind(exitPopupVBox.prefWidthProperty().multiply(0.8));
-        exitLabel.prefHeightProperty().bind(exitPopupVBox.prefWidthProperty().multiply(0.2));
-
-        // Lier la taille et la position des éléments dans creditsPopupVBox
-        closeCreditsButton.prefWidthProperty().bind(creditsPopupVBox.prefWidthProperty().multiply(0.3));
-        closeCreditsButton.prefHeightProperty().bind(creditsPopupVBox.prefHeightProperty().multiply(0.2));
-        creditsTitleLabel.prefWidthProperty().bind(creditsPopupVBox.prefWidthProperty().multiply(0.8));
-        creditsTitleLabel.prefHeightProperty().bind(creditsPopupVBox.prefHeightProperty().multiply(0.2));
-        creditsContentVBox.prefWidthProperty().bind(creditsPopupVBox.prefWidthProperty().multiply(0.8));
-        creditsContentVBox.prefHeightProperty().bind(creditsPopupVBox.prefHeightProperty().multiply(0.6));
-        creditsTextVBox.prefWidthProperty().bind(creditsContentVBox.prefWidthProperty());
-        creditsTextVBox.prefHeightProperty().bind(creditsContentVBox.prefHeightProperty().multiply(0.6));
-
-        // Lier la taille de la police des labels
-        exitLabel.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.02).asString(), "px;"));
-        startGameButton.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.015).asString(), "px;"));
-        settingsButton.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.015).asString(), "px;"));
-        creditsButton.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.015).asString(), "px;"));
-        exitButton.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.015).asString(), "px;"));
-
-        creditsTitleLabel.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.03).asString(), "px;"));
-        credit1.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.015).asString(), "px;"));
-        credit2.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.015).asString(), "px;"));
-        credit3.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.015).asString(), "px;"));
-
         menuView = new MainMenuView();
+        menuView.initializeViewComponents(exitPopupVBox, creditsPopupVBox, creditsTextVBox, creditsContentVBox, mainMenuVBox, loadingVbox,
+                mainPane, backgroundImageView, logoImageView, buttonContainerVBox, startGameButton, settingsButton,
+                creditsButton, exitButton, yesExitButton, noExitButton, closeCreditsButton, exitLabel,
+                creditsTitleLabel, loadingLabel, credit1, credit2, credit3, progressBar, loadingIndicator);
         menuView.playMusic("src/main/resources/com/letop3/ktsh/audio/music/menu.mp3");
+    }
+
+    private Task<Void> createLoadingTask() {
+        return new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                double progress = 0;
+                while (progress < 1.0) {
+                    double randomIncrement = 0.2;
+                    progress += randomIncrement;
+                    updateProgress(progress, 1.0);
+                    Thread.sleep(500);
+                }
+                return null;
+            }
+        };
     }
 
     @FXML
     public void startGame() {
         if (!isPopupOpen) {
-            System.out.println("Game started");
-            menuView.loadNewScene();
-            menuView.stopMusic();
+            menuView.showLoadingScreen();
+            Task<Void> loadingTask = createLoadingTask();
+            menuView.bindProgressBar(loadingTask);
+            loadingTask.setOnSucceeded(event -> {
+                menuView.updateLoadingLabel("Loading Complete");
+                menuView.loadNewScene();
+                menuView.stopMusic();
+            });
+
+            Thread loadingThread = new Thread(loadingTask);
+            loadingThread.setDaemon(true);
+            loadingThread.start();
         }
     }
 
     @FXML
     public void showExit() {
         if (!isPopupOpen) {
-            System.out.println("Exit popup shown");
-            exitPopupVBox.setVisible(true);
+            menuView.showExitPopup();
             isPopupOpen = true;
         }
     }
@@ -134,29 +105,25 @@ public class MainMenuController implements Initializable {
     @FXML
     public void showCredits() {
         if (!isPopupOpen) {
-            System.out.println("Credits shown");
-            creditsPopupVBox.setVisible(true);
+            menuView.showCreditsPopup();
             isPopupOpen = true;
         }
     }
 
     @FXML
     public void hideCredits() {
-        System.out.println("Credits hidden");
-        creditsPopupVBox.setVisible(false);
+        menuView.hideCreditsPopup();
         isPopupOpen = false;
     }
 
     @FXML
     public void hideExit() {
-        System.out.println("Exit popup hidden");
-        exitPopupVBox.setVisible(false);
+        menuView.hideExitPopup();
         isPopupOpen = false;
     }
 
     @FXML
     public void exitGame() {
-        System.out.println("Game exited");
         Platform.exit();
         System.exit(0);
     }
--- src/main/java/com/letop3/ktsh/view/MainMenuView.java
+++ src/main/java/com/letop3/ktsh/view/MainMenuView.java
@@ -1,10 +1,20 @@
 package com.letop3.ktsh.view;
 
+import javafx.beans.binding.Bindings;
+import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,6 +23,137 @@ import java.util.Objects;
 public class MainMenuView {
 
     private MediaPlayer mediaPlayer;
+    private VBox exitPopupVBox, creditsPopupVBox, creditsTextVBox, creditsContentVBox;
+    private VBox mainMenuVBox, loadingVbox;
+    private StackPane mainPane;
+    private ImageView backgroundImageView, logoImageView;
+    private VBox buttonContainerVBox;
+    private Button startGameButton, settingsButton, creditsButton, exitButton;
+    private Button yesExitButton, noExitButton, closeCreditsButton;
+    private Label exitLabel, creditsTitleLabel, loadingLabel;
+    private Label credit1, credit2, credit3;
+    private ProgressBar progressBar;
+    private ProgressIndicator loadingIndicator;
+
+    public void initializeViewComponents(VBox exitPopupVBox, VBox creditsPopupVBox, VBox creditsTextVBox, VBox creditsContentVBox,
+                                         VBox mainMenuVBox, VBox loadingVbox, StackPane mainPane, ImageView backgroundImageView,
+                                         ImageView logoImageView, VBox buttonContainerVBox, Button startGameButton, Button settingsButton,
+                                         Button creditsButton, Button exitButton, Button yesExitButton, Button noExitButton, Button closeCreditsButton,
+                                         Label exitLabel, Label creditsTitleLabel, Label loadingLabel, Label credit1, Label credit2, Label credit3,
+                                         ProgressBar progressBar, ProgressIndicator loadingIndicator) {
+
+        // Initialisation des composants
+        this.exitPopupVBox = exitPopupVBox;
+        this.creditsPopupVBox = creditsPopupVBox;
+        this.creditsTextVBox = creditsTextVBox;
+        this.creditsContentVBox = creditsContentVBox;
+        this.mainMenuVBox = mainMenuVBox;
+        this.loadingVbox = loadingVbox;
+        this.mainPane = mainPane;
+        this.backgroundImageView = backgroundImageView;
+        this.logoImageView = logoImageView;
+        this.buttonContainerVBox = buttonContainerVBox;
+        this.startGameButton = startGameButton;
+        this.settingsButton = settingsButton;
+        this.creditsButton = creditsButton;
+        this.exitButton = exitButton;
+        this.yesExitButton = yesExitButton;
+        this.noExitButton = noExitButton;
+        this.closeCreditsButton = closeCreditsButton;
+        this.exitLabel = exitLabel;
+        this.creditsTitleLabel = creditsTitleLabel;
+        this.loadingLabel = loadingLabel;
+        this.credit1 = credit1;
+        this.credit2 = credit2;
+        this.credit3 = credit3;
+        this.progressBar = progressBar;
+        this.loadingIndicator = loadingIndicator;
+
+        // Appel de la méthode de liaison des composants
+        bindUIComponents();
+    }
+
+    private void bindUIComponents() {
+        // Liaison des propriétés des composants
+        bindImageView(backgroundImageView, mainPane);
+        bindLogoView(logoImageView, mainPane);
+        bindButtonContainer(buttonContainerVBox, mainPane);
+        bindButtons(buttonContainerVBox, startGameButton, settingsButton, creditsButton, exitButton);
+        bindPopups(exitPopupVBox, creditsPopupVBox);
+        bindExitPopupComponents(yesExitButton, noExitButton, exitLabel, exitPopupVBox);
+        bindCreditsPopupComponents(closeCreditsButton, creditsTitleLabel, creditsContentVBox, creditsPopupVBox, creditsTextVBox);
+        bindTextStyles();
+        bindProgressBar(loadingLabel, progressBar, mainPane);
+    }
+
+    private void bindImageView(ImageView imageView, StackPane pane) {
+        imageView.fitWidthProperty().bind(pane.widthProperty());
+        imageView.fitHeightProperty().bind(pane.heightProperty());
+    }
+
+    private void bindLogoView(ImageView logoView, StackPane pane) {
+        logoView.fitWidthProperty().bind(pane.widthProperty().multiply(0.2));
+        logoView.fitHeightProperty().bind(pane.heightProperty().multiply(0.2));
+    }
+
+    private void bindButtonContainer(VBox container, StackPane pane) {
+        container.prefWidthProperty().bind(pane.widthProperty().multiply(0.5));
+        container.prefHeightProperty().bind(pane.heightProperty().multiply(0.4));
+    }
+
+    private void bindButtons(VBox container, Button... buttons) {
+        for (Button button : buttons) {
+            button.prefWidthProperty().bind(container.prefWidthProperty().multiply(0.8));
+            button.prefHeightProperty().bind(container.prefHeightProperty().multiply(0.2));
+        }
+    }
+
+    private void bindPopups(VBox... popups) {
+        for (VBox popup : popups) {
+            popup.prefWidthProperty().bind(mainPane.widthProperty().multiply(0.5));
+            popup.prefHeightProperty().bind(mainPane.heightProperty().multiply(0.4));
+            popup.maxWidthProperty().setValue(800);
+            popup.maxHeightProperty().setValue(400);
+        }
+    }
+
+    private void bindExitPopupComponents(Button yesButton, Button noButton, Label label, VBox popup) {
+        yesButton.prefWidthProperty().bind(popup.prefWidthProperty().multiply(0.3));
+        yesButton.prefHeightProperty().bind(popup.prefHeightProperty().multiply(0.2));
+        noButton.prefWidthProperty().bind(popup.prefWidthProperty().multiply(0.3));
+        noButton.prefHeightProperty().bind(popup.prefHeightProperty().multiply(0.2));
+        label.prefWidthProperty().bind(popup.prefWidthProperty().multiply(0.8));
+        label.prefHeightProperty().bind(popup.prefHeightProperty().multiply(0.2));
+    }
+
+    private void bindCreditsPopupComponents(Button closeButton, Label titleLabel, VBox contentVBox, VBox popupVBox, VBox textVBox) {
+        closeButton.prefWidthProperty().bind(popupVBox.prefWidthProperty().multiply(0.3));
+        closeButton.prefHeightProperty().bind(popupVBox.prefHeightProperty().multiply(0.2));
+        titleLabel.prefWidthProperty().bind(popupVBox.prefWidthProperty().multiply(0.8));
+        titleLabel.prefHeightProperty().bind(popupVBox.prefHeightProperty().multiply(0.2));
+        contentVBox.prefWidthProperty().bind(popupVBox.prefWidthProperty().multiply(0.8));
+        contentVBox.prefHeightProperty().bind(popupVBox.prefHeightProperty().multiply(0.6));
+        textVBox.prefWidthProperty().bind(contentVBox.prefWidthProperty());
+        textVBox.prefHeightProperty().bind(contentVBox.prefHeightProperty().multiply(0.6));
+    }
+
+    private void bindTextStyles() {
+        exitLabel.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.02).asString(), "px;"));
+        startGameButton.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.015).asString(), "px;"));
+        settingsButton.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.015).asString(), "px;"));
+        creditsButton.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.015).asString(), "px;"));
+        exitButton.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.015).asString(), "px;"));
+        creditsTitleLabel.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.03).asString(), "px;"));
+        credit1.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.015).asString(), "px;"));
+        credit2.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.015).asString(), "px;"));
+        credit3.styleProperty().bind(Bindings.concat("-fx-font-size: ", mainPane.widthProperty().multiply(0.015).asString(), "px;"));
+    }
+
+    private void bindProgressBar(Label loadingLabel, ProgressBar progressBar, StackPane pane) {
+        progressBar.prefWidthProperty().bind(pane.widthProperty().multiply(0.6));
+        progressBar.prefHeightProperty().bind(pane.heightProperty().multiply(0.05));
+        loadingLabel.styleProperty().bind(Bindings.concat("-fx-font-size: ", pane.widthProperty().multiply(0.02).asString(), "px;"));
+    }
 
     public void loadNewScene() {
         try {
@@ -25,30 +166,59 @@ public class MainMenuView {
     }
 
     public void playMusic(String musicFile) {
-        // Créer un objet Media
         Media media = new Media(new File(musicFile).toURI().toString());
-
-        // Initialiser le MediaPlayer
         if (mediaPlayer != null) {
             mediaPlayer.stop();
         }
         mediaPlayer = new MediaPlayer(media);
-
-        // Configurer la lecture en boucle
-        mediaPlayer.setOnEndOfMedia(new Runnable() {
-            @Override
-            public void run() {
-                mediaPlayer.seek(javafx.util.Duration.ZERO);
-            }
-        });
-
-        // Jouer la musique
+        mediaPlayer.setOnEndOfMedia(() -> mediaPlayer.seek(Duration.ZERO));
         mediaPlayer.play();
     }
 
     public void stopMusic() {
         if (mediaPlayer != null) {
-            mediaPlayer.stop();
+            for (double i = 1.0; i > 0.0; i -= 0.1) {
+                mediaPlayer.setVolume(i);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+    }
+
+    public void showExitPopup() {
+        exitPopupVBox.setVisible(true);
+    }
+
+    public void hideExitPopup() {
+        exitPopupVBox.setVisible(false);
+    }
+
+    public void showCreditsPopup() {
+        creditsPopupVBox.setVisible(true);
+    }
+
+    public void hideCreditsPopup() {
+        creditsPopupVBox.setVisible(false);
+    }
+
+    public void showLoadingScreen() {
+        mainMenuVBox.setVisible(false);
+        loadingVbox.setVisible(true);
+    }
+
+    public void hideLoadingScreen() {
+        loadingVbox.setVisible(false);
+        mainMenuVBox.setVisible(true);
+    }
+
+    public void bindProgressBar(Task<Void> task) {
+        progressBar.progressProperty().bind(task.progressProperty());
+    }
+
+    public void updateLoadingLabel(String text) {
+        loadingLabel.setText(text);
     }
 }
