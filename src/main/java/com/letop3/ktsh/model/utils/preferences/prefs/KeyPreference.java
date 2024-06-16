cf7bb2f1fa926192723ff6e8e8d04890d13d8dcf
aagogue <164860332+aagogue@users.noreply.github.com>
2024-06-16 22:32:00 +0200

    changes sur la manière de gerer les preferences
    Signed-off-by: aagogue <164860332+aagogue@users.noreply.github.com>

--- src/main/java/com/letop3/ktsh/model/utils/preferences/prefs/KeyPreference.java
+++ src/main/java/com/letop3/ktsh/model/utils/preferences/prefs/KeyPreference.java
@@ -2,14 +2,33 @@ package com.letop3.ktsh.model.utils.preferences.prefs;
 
 import javafx.scene.input.KeyCode;
 
-public class KeyPreference {
-    public static KeyCode MOVE_UP = KeyCode.Z;
-    public static KeyCode MOVE_DOWN = KeyCode.S;
-    public static KeyCode MOVE_RIGHT = KeyCode.D;
-    public static KeyCode MOVE_LEFT = KeyCode.Q;
-    public static KeyCode QUICK_SLOT = KeyCode.A;
-    public static KeyCode INVENTORY = KeyCode.E;
-    public static KeyCode INTERACT = KeyCode.F;
-    public static KeyCode ATTACK = KeyCode.SPACE;
-    public static KeyCode SHIELD = KeyCode.SHIFT;
+public enum KeyPreference {
+    MOVE_UP("moveUp", KeyCode.Z, KeyCode.UP),
+    MOVE_DOWN("moveDown", KeyCode.S, KeyCode.DOWN),
+    MOVE_RIGHT("moveRight", KeyCode.D, KeyCode.RIGHT),
+    MOVE_LEFT("moveLeft", KeyCode.Q, KeyCode.LEFT),
+    QUICK_SLOT("quickSlot", KeyCode.A, KeyCode.DIGIT1),
+    INVENTORY("inventory", KeyCode.E, KeyCode.I),
+    INTERACT("interact", KeyCode.F, KeyCode.ENTER),
+    ATTACK("attack", KeyCode.SPACE, KeyCode.SPACE),
+    SHIELD("shield", KeyCode.SHIFT, KeyCode.SHIFT);
+
+    public final String key;
+    public final KeyCode value;
+    public final KeyCode defaultValue;
+
+    KeyPreference(String key, KeyCode value, KeyCode defaultValue) {
+        this.key = key;
+        this.value = value;
+        this.defaultValue = defaultValue;
+    }
+
+    public KeyCode getKeyCode() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return key;
+    }
 }
