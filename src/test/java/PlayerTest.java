fa3037ed9711a8be54b6ec6d99dbb956aaa8d8c4
aagogue <164860332+aagogue@users.noreply.github.com>
2024-06-16 22:39:56 +0200

    base pour les tests
    Signed-off-by: aagogue <164860332+aagogue@users.noreply.github.com>

---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+
+    @Test
+    void testPlayerCreation() {
+
+    }
+
+    @Test
+    void testPlayerScoreUpdate() {
+    }
+
+    @Test
+    void testPlayerNameUpdate() {
+
+    }
+}
